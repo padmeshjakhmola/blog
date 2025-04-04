f21072db850b3e9d00b291d9987cffee418575a8
Padmesh Jakhmola <prankshadow@gmail.com>
2025-04-04 20:09:14 +0530

    feat: homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import BlogCard from "@/components/BlogCard";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,7 +13,40 @@ export default function Home() {
           walks of life.
         </p>
       </div>
-      <div className="top-heading-small flex flex-row justify-between items-center">
+
+      <div className="relative w-full max-h-[400px] md:max-h-[500px] lg:max-h-[600px] overflow-hidden rounded-2xl transition-all hover:scale-102 cursor-pointer shadow-xl">
+        <Image
+          src="/assets/images/person_reading.jpg"
+          alt="main_image"
+          width={1200}
+          height={600}
+          className="w-full h-auto object-cover"
+        />
+
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black/40 flex flex-col justify-end p-6 text-white ">
+          <h1 className="text-2xl md:text-4xl font-bold hover:underline">
+            Designing for Accessibility: A Guide
+          </h1>
+          <p className="text-sm md:text-lg mt-2 hover:underline">
+            A comprehensive guide to making web and app designs more accessible
+            for people with disabilities, ensuring inclusivity.,
+          </p>
+
+          <div className="flex flex-row py-10 no-underline gap-16">
+            <div className="space-y-2">
+              <p className="text-sm">Written By</p>
+              <p>Prank Shadow</p>
+            </div>
+            <div className="space-y-2">
+              <p className="text-sm">Published On</p>
+              <p>10 April 2025</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="top-heading-small flex flex-row justify-between items-center pt-10">
         <h1 className="font-bold text-lg">Featured Blog Post</h1>
         <Button
           variant="secondary"
@@ -21,6 +55,7 @@ export default function Home() {
           View all posts
         </Button>
       </div>
+
       <div className="py-8">
         <BlogCard />
       </div>
