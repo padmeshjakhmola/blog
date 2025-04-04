e1f435d8ae286ae19e8c73217a063d2217c7f27d
Padmesh Jakhmola <prankshadow@gmail.com>
2025-04-04 19:47:07 +0530

    feat: added heading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,14 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="p-10">
+      <div className="py-10 space-y-2">
+        <h1 className="text-4xl font-semibold">{`"Voices of the Web – A Blog for Everyone"`}</h1>
+        <p className="text-gray-800">
+          A space where thoughts, stories, and ideas come to life. Explore
+          diverse topics, insightful articles, and engaging discussions from all
+          walks of life.
+        </p>
+      </div>
       <div className="top-heading-small flex flex-row justify-between items-center">
         <h1 className="font-bold text-lg">Featured Blog Post</h1>
         <Button
---
+++ components/NavBar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
+
+const NavBar = () => {
+  return (
+    <div className="flex flex-row justify-between mx-10 mt-6 text-md">
+      <Image
+        src="/assets/images/logo.png"
+        alt="logo"
+        width={50}
+        height={50}
+        className="block"
+      />
+      <div className="flex flex-row justify-center items-center space-x-14 font-semibold">
+        <p className="hover:underline cursor-pointer">Home</p>
+        <p className="hover:underline cursor-pointer">Products</p>
+        <p className="hover:underline cursor-pointer">About</p>
+
+        <Button
+          variant="secondary"
+          className="bg-brand hover:bg-brand-dark/20 text-brand-dark font-bold hover:underline cursor-pointer"
+        >
+          Get Started
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
